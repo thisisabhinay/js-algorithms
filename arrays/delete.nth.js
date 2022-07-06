33951f1e242b035bfe685d7def6faddb714dc0cb
thisisabhinay <a8h1nay@gmail.com>
2022-07-06 12:19:36 +0530

    Setup project

---
+++ arrays/delete.nth.js
@@ -0,0 +1,18 @@
+/*
+    Given a list lst and a number N, create a new list
+    that contains each number of the list at most N times without reordering.
+    
+    For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], 
+    drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, 
+    which leads to [1,2,3,1,2,3]
+*/
+
+//  Time complexity O(n^2)
+const deleteNthNaive = (array, n) => {
+    let newArr = []
+    for(num in Array) {
+
+    } 
+}
+
+//  Time complexit O(n), using hash table
