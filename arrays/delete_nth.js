5c7f375424d06bae958f8e224cf906120cbfe85b
thisisabhinay <a8h1nay@gmail.com>
2022-07-06 13:27:24 +0530

    Creates deleteNthNaive()

--- arrays/delete_nth.js
+++ arrays/delete_nth.js
@@ -1,3 +1,4 @@
+require('../utils');
 /*
     Given a list lst and a number N, create a new list
     that contains each number of the list at most N times without reordering.
@@ -8,11 +9,16 @@
 */
 
 //  Time complexity O(n^2)
-const deleteNthNaive = (array, n) => {
+const deleteNthNaive = (array, N) => {
     let newArr = []
-    for(num in Array) {
-
-    } 
+    for(let num of array) {
+        if(array.count(num) < N) newArr.push(num)
+    }
+    
+    return newArr
 }
+
+// Runnning examples:
+console.log("deleteNthNaive([1,2,3,1,2,1,2,3], 2) -> ", deleteNthNaive([1,2,3,1,2,1,2,3], 2))
 
 //  Time complexit O(n), using hash table
